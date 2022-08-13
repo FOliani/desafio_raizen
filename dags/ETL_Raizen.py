d81ba00fcf33cb37fa9dcb0455e9698baf52dc05
Fabio Oliani <79013371+FOliani@users.noreply.github.com>
2022-08-13 12:34:19 -0300

    small correction

--- dags/ETL_Raizen.py
+++ dags/ETL_Raizen.py
@@ -151,7 +151,7 @@ def create_database_from_parquet():
         logging.info(f"Check completed successfully")
 
 
-with DAG('ETL_Raízen', start_date = datetime.datetime(2022,8,9),
+with DAG('ETL_Raizen', start_date = datetime.datetime(2022,8,9),
            schedule_interval = '30 * * * *' , catchup = False) as dag:
 
     create_raw_data_dir = PythonOperator(
